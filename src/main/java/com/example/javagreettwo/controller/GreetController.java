468d753d32f6490167f07ad6f78f139a6b03ae03
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-19 11:23:28 -0400

    Creada solicitud POST /greet a partir de un objeto opcional dado como argumento

--- src/main/java/com/example/javagreettwo/controller/GreetController.java
+++ src/main/java/com/example/javagreettwo/controller/GreetController.java
@@ -1,8 +1,11 @@
 package com.example.javagreettwo.controller;
 
 import com.example.javagreettwo.entity.Greet;
+import com.example.javagreettwo.entity.Person;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,8 +20,15 @@ public class GreetController {
 
   @GetMapping
   @ResponseBody
-  public Greet personalizedGreet(@RequestParam(name = "name", required = false) String name) {
+  public Greet getPersonalizedGreet(@RequestParam(name = "name", required = false) String name) {
     String greet = String.format(template, (name != null) ? name : "mundo");
+    return new Greet(counter.incrementAndGet(), greet);
+  }
+
+  @PostMapping
+  @ResponseBody
+  public Greet postPersonalizedGreet(@RequestBody(required = false) Person person) {
+    String greet = String.format(template, (person != null) ? person.getName() : "mundo");
     return new Greet(counter.incrementAndGet(), greet);
   }
 
