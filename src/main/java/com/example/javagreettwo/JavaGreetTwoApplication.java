f0f8c4b9dd480f47f13e4a5dc32dbe49940a3181
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-19 11:02:31 -0400

    Primer commit

---
+++ src/main/java/com/example/javagreettwo/JavaGreetTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javagreettwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGreetTwoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaGreetTwoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/javagreettwo/JavaGreetTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javagreettwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGreetTwoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
